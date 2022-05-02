2800f78ae217cfc6b509d723bf4cbe88a1bdf105
badrimanishankar <92393046+badrimanishankar@users.noreply.github.com>
2022-05-02 16:47:03 -0400

    Add files via upload

---
+++ SQLCode/movieTrigger.sql
@@ -0,0 +1,8 @@
+Create Trigger addReview
+After Insert 
+on review
+FOR EACH ROW
+UPDATE movie
+SET movie.rating = (SELECT AVG(rating) FROM review
+			WHERE movie.mid = review.mid)
+WHERE RID = NEW.RID 
