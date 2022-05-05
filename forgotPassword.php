8318192276ea3b5dd6a44ccc6a07c45417587ab3
CamLahey <camlahey1@gmail.com>
2022-05-05 19:24:11 -0400

    Changed email because someone stole it

--- forgotPassword.php
+++ forgotPassword.php
@@ -28,15 +28,15 @@ if(isset($_POST["email"])){
         $mail->isSMTP();                                            //Send using SMTP
         $mail->Host = 'smtp.gmail.com';                     //Set the SMTP server to send through
         $mail->SMTPAuth = true;                                   //Enable SMTP authentication
-        $mail->Username = 'rottenorangeinc@gmail.com';                     //SMTP username
-        $mail->Password = 'Database1*';                               //SMTP password
+        $mail->Username = 'orangerotteninc@gmail.com';                     //SMTP username
+        $mail->Password = 'Database12*';                               //SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
         $mail->Port = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
         //Recipients
-        $mail->setFrom('rottenorangeinc@gmail.com', 'Rotten Orange Inc');
+        $mail->setFrom('orangerotteninc@gmail.com', 'Rotten Orange Inc');
         $mail->addAddress($emailTo);
-        $mail->addReplyTo('rottenorangeinc@gmail.com', 'Rotten Orange Inc');
+        $mail->addReplyTo('orangerotteninc@gmail.com', 'Rotten Orange Inc');
 
 //    //Attachments
 //    $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
