55407a877da518729fbf73f85e5ea6e90cf494f6
Junyi-Pan <63979063+Junyi-Pan@users.noreply.github.com>
2022-05-09 17:31:34 -0400

    Update movielog.sql

--- SQLCode/movielog.sql
+++ SQLCode/movielog.sql
@@ -2,10 +2,10 @@
 -- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: May 02, 2022 at 10:34 PM
--- Server version: 8.0.28
--- PHP Version: 8.1.1
+-- Host: localhost
+-- Generation Time: May 09, 2022 at 11:30 PM
+-- Server version: 10.4.20-MariaDB
+-- PHP Version: 7.4.22
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -28,7 +28,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `admin` (
-  `AID` int NOT NULL,
+  `AID` int(11) NOT NULL,
   `Username` varchar(255) NOT NULL,
   `Password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -47,15 +47,15 @@ INSERT INTO `admin` (`AID`, `Username`, `Password`) VALUES
 --
 
 CREATE TABLE `movie` (
-  `MID` int NOT NULL,
+  `MID` int(11) NOT NULL,
   `Name` varchar(255) NOT NULL,
   `Genre` varchar(255) NOT NULL,
   `Director` varchar(255) NOT NULL,
   `Summary` varchar(1000) NOT NULL,
   `Date` date NOT NULL,
-  `AID` int NOT NULL,
+  `AID` int(11) NOT NULL,
   `Image` varchar(255) DEFAULT NULL,
-  `Rating` int NOT NULL
+  `Rating` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -71,10 +71,28 @@ INSERT INTO `movie` (`MID`, `Name`, `Genre`, `Director`, `Summary`, `Date`, `AID
 (6, 'High School Musical', 'Musical', 'Kenny Ortega', 'Troy Bolton (Zac Efron), the star athlete at a small-town high school, falls for nerdy beauty Gabriella Montez (Vanessa Anne Hudgens) at a holiday karaoke party. When they return to campus, Troy and Gabriella audition for the upcoming school musical. Meanwhile, the jealous Sharpay Evans (Ashley Tisdale) conspires to squelch their chances. The two must struggle to make it to auditions while also meeting their existing obligations to the basketball team and the academic decathlon.', '2006-01-20', 1, 'https://resizing.flixster.com/A6qvS7o91Q-UdstPZ3kdsjo9GQc=/206x305/v2/https://flxt.tmsimg.com/assets/p160369_p_v8_ak.jpg', 0),
 (7, 'Get Out', 'Horror', 'Jordan Peele', 'Now that Chris and his girlfriend, Rose, have reached the meet-the-parents milestone of dating, she invites him for a weekend getaway upstate with her parents, Missy and Dean. At first, Chris reads the family overly accommodating behaviour as nervous attempts to deal with their daughter interracial relationship, but as the weekend progresses, a series of increasingly disturbing discoveries leads him to a truth that he never could have imagined.', '2017-02-24', 1, 'https://resizing.flixster.com/WQvxDGdZxKzj53GK3fVbkC-2eC8=/206x305/v2/https://resizing.flixster.com/Q2uwvQLSNT5ObCtXKALBjceWDbw=/ems.ZW1zLXByZC1hc3NldHMvbW92aWVzLzNiZGVhNTVmLWQzOGUtNGMwZC1hYmZjLTIxZWE5NThkMTdjNS53ZWJw', 0),
 (8, 'The Dark Knight', 'Fantasy/Adventure', 'Christopher Nolan', 'With the help of allies Lt. Jim Gordon (Gary Oldman) and DA Harvey Dent (Aaron Eckhart), Batman (Christian Bale) has been able to keep a tight lid on crime in Gotham City. But when a vile young criminal calling himself the Joker (Heath Ledger) suddenly throws the town into chaos, the caped Crusader begins to tread a fine line between heroism and vigilantism.', '2008-07-18', 1, 'https://resizing.flixster.com/WAHXGKleT3QvhqHUlFGIRgcQAjU=/206x305/v2/https://flxt.tmsimg.com/assets/p173378_p_v8_au.jpg', 0),
-(9, 'Star Wars: Episode V -- The Empire Strikes Back', 'Fantasy/Adventure', 'Irvin Kershner', 'The adventure continues in this \"Star Wars\" sequel. Luke Skywalker (Mark Hamill), Han Solo (Harrison Ford), Princess Leia (Carrie Fisher) and Chewbacca (Peter Mayhew) face attack by the Imperial forces and its AT-AT walkers on the ice planet Hoth. While Han and Leia escape in the Millennium Falcon, Luke travels to Dagobah in search of Yoda. Only with the Jedi master''s help will Luke survive when the dark side of the Force beckons him into the ultimate duel with Darth Vader (David Prowse).', '1980-05-21', 1, 'https://resizing.flixster.com/V_oZJXOwGIr2tZpJzbtv43zdFnk=/206x305/v2/https://flxt.tmsimg.com/assets/p8884_p_v8_ae.jpg', 0),
+(9, 'Star Wars: Episode V -- The Empire Strikes Back', 'Fantasy/Adventure', 'Irvin Kershner', 'The adventure continues in this \"Star Wars\" sequel. Luke Skywalker (Mark Hamill), Han Solo (Harrison Ford), Princess Leia (Carrie Fisher) and Chewbacca (Peter Mayhew) face attack by the Imperial forces and its AT-AT walkers on the ice planet Hoth. While Han and Leia escape in the Millennium Falcon, Luke travels to Dagobah in search of Yoda. Only with the Jedi master\'s help will Luke survive when the dark side of the Force beckons him into the ultimate duel with Darth Vader (David Prowse).', '1980-05-21', 1, 'https://resizing.flixster.com/V_oZJXOwGIr2tZpJzbtv43zdFnk=/206x305/v2/https://flxt.tmsimg.com/assets/p8884_p_v8_ae.jpg', 0),
 (10, 'Whiplash', 'Drama/Music', 'Damien Chazelle', 'Andrew Neiman (Miles Teller) is an ambitious young jazz drummer, in pursuit of rising to the top of his elite music conservatory. Terence Fletcher (J.K. Simmons), an instructor known for his terrifying teaching methods, discovers Andrew and transfers the aspiring drummer into the top jazz ensemble, forever changing the young man\'s life. But Andrew\'s passion to achieve perfection quickly spirals into obsession, as his ruthless teacher pushes him to the brink of his ability and his sanity.', '2014-10-10', 1, 'https://resizing.flixster.com/9vHaXzbOKznuRv-w-jOJQAtgjdw=/206x305/v2/https://flxt.tmsimg.com/assets/p10488558_p_v12_ai.jpg', 0),
 (11, 'American Psycho', 'Comedy/Horror', 'Mary Harron', 'In New York City in 1987, a handsome, young urban professional, Patrick Bateman (Christian Bale), lives a second life as a gruesome serial killer by night. The cast is filled by the detective (Willem Dafoe), the fiance (Reese Witherspoon), the mistress (Samantha Mathis), the coworker (Jared Leto), and the secretary (Chloë Sevigny). This is a biting, wry comedy examining the elements that make a man a monster.', '2000-04-14', 1, 'https://resizing.flixster.com/VugRiwPMAuksEKklXBz3_nKRtJg=/206x305/v2/https://flxt.tmsimg.com/assets/p24116_p_v12_at.jpg', 0),
 (12, 'Frank', 'Drama/Comedy', 'Lenny Abrahamson', 'An aspiring musician (Domhnall Gleeson) finds himself way out of his element after he joins a pop group led by an enigmatic figure (Michael Fassbender) who wears a giant fake head.', '2014-08-15', 1, 'https://resizing.flixster.com/5HR4YF6UymZM_yiKrcimhIqaOcA=/206x305/v2/https://flxt.tmsimg.com/assets/p10688612_p_v8_ad.jpg', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Stand-in structure for view `movieview`
+-- (See below for the actual view)
+--
+CREATE TABLE `movieview` (
+`MID` int(11)
+,`Name` varchar(255)
+,`Genre` varchar(255)
+,`Director` varchar(255)
+,`Summary` varchar(1000)
+,`Date` date
+,`AID` int(11)
+,`Image` varchar(255)
+,`Rating` int(11)
+);
 
 -- --------------------------------------------------------
 
@@ -83,7 +101,7 @@ INSERT INTO `movie` (`MID`, `Name`, `Genre`, `Director`, `Summary`, `Date`, `AID
 --
 
 CREATE TABLE `resetpassword` (
-  `rpid` int NOT NULL,
+  `rpid` int(11) NOT NULL,
   `code` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -95,11 +113,11 @@ CREATE TABLE `resetpassword` (
 --
 
 CREATE TABLE `review` (
-  `RID` int NOT NULL,
-  `Text` varchar(2000)NOT NULL,
-  `UID` int NOT NULL,
-  `MID` int NOT NULL,
-  `Rating` int NOT NULL
+  `RID` int(11) NOT NULL,
+  `Text` varchar(2000) NOT NULL,
+  `UID` int(11) NOT NULL,
+  `MID` int(11) NOT NULL,
+  `Rating` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -115,6 +133,16 @@ INSERT INTO `review` (`RID`, `Text`, `UID`, `MID`, `Rating`) VALUES
 (6, 'test', 1, 1, 4),
 (7, 'test', 1, 1, 4);
 
+--
+-- Triggers `review`
+--
+DELIMITER $$
+CREATE TRIGGER `addReview` AFTER INSERT ON `review` FOR EACH ROW UPDATE movie
+SET movie.rating = (SELECT AVG(rating) FROM review
+			WHERE movie.mid = review.mid)
+$$
+DELIMITER ;
+
 -- --------------------------------------------------------
 
 --
@@ -122,7 +150,7 @@ INSERT INTO `review` (`RID`, `Text`, `UID`, `MID`, `Rating`) VALUES
 --
 
 CREATE TABLE `user` (
-  `uID` int NOT NULL,
+  `uID` int(11) NOT NULL,
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL
@@ -134,6 +162,15 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`uID`, `username`, `password`, `email`) VALUES
 (1, 'CamLahey', '42d3e5c8573c0a0508c39dd6e9641836fc6ab41c03d7e380120f59b20805fc1cb2f43cc930ba31ac89172e57717706d27612e34315f7952746cf282d56f54613', 'camlahey@gmail.com');
+
+-- --------------------------------------------------------
+
+--
+-- Structure for view `movieview`
+--
+DROP TABLE IF EXISTS `movieview`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `movieview`  AS SELECT `movie`.`MID` AS `MID`, `movie`.`Name` AS `Name`, `movie`.`Genre` AS `Genre`, `movie`.`Director` AS `Director`, `movie`.`Summary` AS `Summary`, `movie`.`Date` AS `Date`, `movie`.`AID` AS `AID`, `movie`.`Image` AS `Image`, `movie`.`Rating` AS `Rating` FROM `movie` ;
 
 --
 -- Indexes for dumped tables
@@ -180,31 +217,31 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `AID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `AID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `movie`
 --
 ALTER TABLE `movie`
-  MODIFY `MID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `MID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `resetpassword`
 --
 ALTER TABLE `resetpassword`
-  MODIFY `rpid` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `rpid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `review`
 --
 ALTER TABLE `review`
-  MODIFY `RID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `RID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `uID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `uID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Constraints for dumped tables
